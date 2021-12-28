9bc32ade2dc72ba009ce6e50cf65829921e12d4f
user.email <laneherberholz@gmail.com>
2021-12-28 00:51:32 +0000

    Have time centered on status display

--- client/src/components/GameScreen/StatusDisplay.jsx
+++ client/src/components/GameScreen/StatusDisplay.jsx
@@ -1,10 +1,44 @@
-import React from "react";
-import { Box } from '@mui/material';
+import React, { useEffect, useState, useRef } from "react";
+import { Box, FormControl, InputLabel, Select, Tooltip } from '@mui/material';
+import useStopwatch from "../../hooks/useStopwatch";
 
 const StatusDisplay = () => {
+    const { time, startTimer, stopTimer } = useStopwatch();
+    const sWatchContainer = useRef(null);
+
+    useEffect(() => {
+        startTimer();
+    }, []);
+
+    useEffect(() => {
+
+        //have to wait just a bit because the timer isn't fully rendered in right away?
+        setTimeout(() => {
+            //get width of stopwatch and container
+            const stopwatchWidth = Math.ceil(sWatchContainer.current.firstChild.getBoundingClientRect().width - 5);
+            const containerWidth = Math.ceil(sWatchContainer.current.getBoundingClientRect().width);
+            const emptySpace = containerWidth - stopwatchWidth;
+            console.log(containerWidth, stopwatchWidth, (emptySpace/2));
+
+            //take half of the empty space left and make it the left margin of the stopwatch span
+            sWatchContainer.current.firstChild.style.marginLeft = `${emptySpace / 2}px`;
+        }, 100);
+
+    }, [sWatchContainer]);
+
     return (
         <Box className={"statusContainer"}>
-            
+            <Box className={"stopwatchContainer statusBox"} ref={sWatchContainer}>
+                <span>
+                    {time.minutes}:{time.seconds}:{time.ms}
+                </span>
+            </Box>
+            <Box className={"historySelectContainer statusBox"}>
+                
+            </Box>
+            <Box className={"moveCountContainer statusBox"}>
+                
+            </Box>
         </Box>
     );
 };
